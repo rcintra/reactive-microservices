1955217120bb8f791c837f719326b385271ae79c
Rafael Cintra <ra.cintra@gmail.com>
2018-12-14 06:49:34 -0200

    hello consumer

---
+++ hello-consumer-microservice-http/src/main/java/com/rcintra/book/http/HelloConsumerMicroservice.java
@@ -0,0 +1,12 @@
+package com.rcintra.book.http;
+
+import io.vertx.core.AbstractVerticle;
+
+public class HelloConsumerMicroservice extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
